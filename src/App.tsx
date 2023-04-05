4c854185b481a74555abc0ce0a78d0558d5ebe7b
yujin <yjcho@cavyos.com>
2023-04-05 17:38:24 +0900

    feat: :sparkles: make group editable with db click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const plugSVG = (
@@ -19,9 +19,14 @@ const plugSVG = (
 );
 
 class Task {
-  constructor(public title: string, public done: boolean = false) {
+  constructor(
+    public title: string,
+    public done: boolean = false,
+    public isEditing: boolean = false
+  ) {
     this.title = title;
     this.done = done;
+    this.isEditing = isEditing;
   }
   modify(title: string) {
     this.title = title;
@@ -33,58 +38,79 @@ class Task {
 }
 
 class Group extends Array<Task> {
-  constructor(public title: string, tasks: Task[] = []) {
+  isEditing: boolean;
+  constructor(
+    public title: string,
+    tasks: Task[] = [],
+    isEditing: boolean = true
+  ) {
     super(...tasks);
     this.title = title;
+    this.isEditing = isEditing;
   }
   removeTask(index: number) {
     this.splice(index, 1);
   }
 }
 
-function showConffetti() {
-  return (
-    <div className="absolute top-0 left-0 w-full h-full">
-      <canvas
-        id="canvas"
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          zIndex: 9999,
-        }}
-      ></canvas>
-    </div>
-  );
-}
-
 function App() {
-  const [workingGroup, setWorkingGroup] = useState<Group>(new Group("Today"));
+  const [workingGroup, setWorkingGroup] = useState<Group>(
+    new Group("Today", [], false)
+  );
 
   const [groups, setGroup] = useState<Group[]>([
-    new Group("1", [
-      new Task("Get up"),
-      new Task("Brush teeth"),
-      new Task("Eat breakfast"),
-    ]),
-
-    new Group("2", [
-      new Task("Get up"),
-      new Task("Brush teeth"),
-      new Task("Eat breakfast"),
-    ]),
-    new Group("3", [
-      new Task("Get up"),
-      new Task("Brush teeth"),
-      new Task("Eat breakfast"),
-    ]),
+    new Group(
+      "Todo",
+      [new Task("Get up"), new Task("Brush teeth"), new Task("Eat breakfast")],
+      false
+    ),
   ]);
 
-  const DrawGroup = (props: { group: Group; index?: number }) => {
-    const { group, index } = props;
+  const DrawGroup = (props: { group: Group; index?: number; add: boolean }) => {
+    const { group, index, add } = props;
+    const [textTitle, setTextTitle] = useState<string>(group.title);
+    const titleInput = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      titleInput.current?.focus();
+    });
+
     return (
-      <article className=" w-40">
-        <h2 className="text-lg mb-2">{group.title}</h2>
+      <article className="w-40">
+        {group.isEditing ? (
+          <input
+            className="text-lg mb-2 outline-1 outline w-40 rounded"
+            type="text"
+            value={textTitle}
+            ref={titleInput}
+            onChange={(e) => {
+              setTextTitle(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                group.title = textTitle;
+                group.isEditing = false;
+                setGroup([...groups]);
+              }
+            }}
+            onBlur={() => {
+              group.title = textTitle;
+              group.isEditing = false;
+              setGroup([...groups]);
+            }}
+          />
+        ) : (
+          <h2
+            className="text-lg mb-2"
+            onDoubleClick={() => {
+              group.isEditing = true;
+              setGroup([...groups]);
+            }}
+          >
+            {group.title}
+          </h2>
+        )}
+
         <div className="flex flex-col gap-1">
           {group.map((task, i) => (
             <div
@@ -95,12 +121,27 @@ function App() {
               <p className=" text-slate-700">{task.title}</p>
             </div>
           ))}
-          <button className="flex items-center justify-center bg-slate-500 rounded p-2 opacity-30 hover:opacity-70">
-            {plugSVG}
-          </button>
+          {add && (
+            <button className="flex items-center justify-center bg-slate-500 rounded p-2 opacity-30 hover:opacity-70">
+              {plugSVG}
+            </button>
+          )}
         </div>
+        {group.length === 0 && add === false && (
+          <h2 className="text-3xl">Let's do something.</h2>
+        )}
       </article>
     );
+  };
+
+  const toggleTask = (index: number) => {
+    workingGroup[index].toggle();
+    setWorkingGroup(workingGroup);
+  };
+
+  const addNewTask = (task: Task) => {
+    workingGroup.push(task);
+    setWorkingGroup(workingGroup);
   };
 
   return (
@@ -111,11 +152,11 @@ function App() {
         </h1>
       </header>
       <main className="mb-10 flex justify-stretch items-center">
-        <DrawGroup group={workingGroup} />
+        <DrawGroup group={workingGroup} add={false} />
       </main>
       <main className="flex flex-row gap-4 relative">
         {groups.map((group, index) => (
-          <DrawGroup group={group} index={index} key={index} />
+          <DrawGroup group={group} index={index} key={index} add={true} />
         ))}
         <button
           className="absolute left-full ml-4 h-full flex flex-col justify-center items-center bg-slate-500 rounded p-2 opacity-30 hover:opacity-70"
